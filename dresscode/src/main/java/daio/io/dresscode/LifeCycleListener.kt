4d6dce14882242ca339886c7fa9c186810990919
Daio-io <dle.williams1@gmail.com>
2020-06-07 21:52:04 +0100

    Kotlin updaate and other tweaks

--- dresscode/src/main/java/daio/io/dresscode/LifeCycleListener.kt
+++ dresscode/src/main/java/daio/io/dresscode/LifeCycleListener.kt
@@ -6,28 +6,21 @@ import android.os.Bundle
 
 internal class LifeCycleListener : Application.ActivityLifecycleCallbacks {
 
-    override fun onActivityPaused(activity: Activity?) {
-
-    }
+    override fun onActivityPaused(activity: Activity?) {}
 
     override fun onActivityResumed(activity: Activity) {
         val dressCodeOnActivity = activities[activity::class] ?: return
         if (currentDressCode != dressCodeOnActivity) activity.recreate()
     }
 
-    override fun onActivityStarted(activity: Activity?) {
-    }
+    override fun onActivityStarted(activity: Activity?) {}
 
-    override fun onActivityDestroyed(activity: Activity?) {
-    }
+    override fun onActivityDestroyed(activity: Activity?) {}
 
-    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {
-    }
+    override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {}
 
-    override fun onActivityStopped(activity: Activity?) {
-    }
+    override fun onActivityStopped(activity: Activity?) {}
 
-    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {
-    }
+    override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {}
 
 }
